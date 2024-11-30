f09b8de062010d91f89c39e10c8d3cab15ccb723
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-11-30 21:13:43 +0000

    Menu ingresar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,12 +6,62 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light andaBackgroundColor h-100">
 			<div className="container">
 				<Link to="/">
-					<img src="https://i.ibb.co/zrCKh8H/logo-anda-blanco.png" alt="Logo ANDA" style={{height: "10vh", width: "15vh"}}></img>
+					<img
+						src="https://i.ibb.co/zrCKh8H/logo-anda-blanco.png"
+						alt="Logo ANDA"
+						style={{ height: "10vh", width: "15vh" }}
+					/>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-light andaTextColor">Ingresar</button>
-					</Link>
+					<div className="dropdown">
+						<button
+							className="btn btn-light andaTextColor dropdown-toggle"
+							type="button"
+							id="dropdownMenuButton"
+							data-bs-toggle="dropdown"
+							aria-expanded="false"
+						>
+							Ingresar
+						</button>
+						<div
+							className="dropdown-menu dropdown-menu-end p-4 shadow"
+							aria-labelledby="dropdownMenuButton"
+							style={{ minWidth: "300px" }} // Hacer el menú más ancho
+						>
+							<form>
+								<div className="mb-3">
+									<label htmlFor="email" className="form-label">
+										Email
+									</label>
+									<input
+										type="email"
+										className="form-control"
+										id="email"
+										placeholder="Email"
+										required
+									/>
+								</div>
+								<div className="mb-3">
+									<label htmlFor="password" className="form-label">
+										Contraseña
+									</label>
+									<input
+										type="password"
+										className="form-control"
+										id="password"
+										placeholder="Contraseña"
+										required
+									/>
+								</div>
+								<a href="#" className="d-block mb-3 text-decoration-none">
+									Olvidé mi contraseña
+								</a>
+								<button type="submit" className="btn btn-primary w-100">
+									Ingresar
+								</button>
+							</form>
+						</div>
+					</div>
 				</div>
 			</div>
 		</nav>
